2cea1bc141f9cbf40e067b14b026fcef8c5267c5
Machadoazul <112534393+Machadoazul@users.noreply.github.com>
2022-09-16 14:43:48 -0300

    Gabriel Borges

--- NLW/web/src/App.tsx
+++ NLW/web/src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/main.css';
-import logoImg from './Assets/Logo-nlw-esports.svg'
+import logoImg from './Assets/Logo-nlw-esports.svg';
 
 function App() {
   return (
@@ -7,17 +7,19 @@ function App() {
     <img src={logoImg} alt="" />
 
       <h1 className="text-6xl text-white font-black mt-20">
-        Seu <span className="bg-clip-text bg-nlw-gradient">duo</span> está aqui.
+        Seu <span className="bg-clip-text text-transparent bg-nlw-gradient">duo</span> está aqui.
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-20">
+
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-1.png" alt=""/> 
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+          <div className="text w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
              <strong className="font-bold text-white block">League of Legends</strong>
             <span className=" text-zinc-300 text-sm block">4 Anúncios</span>
           </div>
         </a>
+        
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-2.png" alt=""/> 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
@@ -25,6 +27,7 @@ function App() {
             <span className=" text-zinc-300 text-sm block">4 Anúncios</span>
           </div>
         </a>
+
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-3.png" alt=""/> 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
@@ -32,6 +35,7 @@ function App() {
             <span className=" text-zinc-300 text-sm block">4 Anúncios</span>
           </div>
         </a>
+
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-4.png" alt=""/> 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
@@ -39,6 +43,7 @@ function App() {
             <span className=" text-zinc-300 text-sm block">4 Anúncios</span>
           </div>
         </a>
+        
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-5.png" alt=""/> 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
@@ -46,18 +51,20 @@ function App() {
             <span className=" text-zinc-300 text-sm block">4 Anúncios</span>
           </div>
         </a>
+
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-6.png" alt=""/> 
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
              <strong className="font-bold text-white block">Fortnite</strong>
             <span className=" text-zinc-300 text-sm block">4 Anúncios</span>
           </div>
-        </a>
-
-
-        
-          
+        </a>    
       </div>
+
+
+
+
+
     </div>
   )
 }
--- NLW/web/tailwind.config.cjs
+++ NLW/web/tailwind.config.cjs
@@ -7,12 +7,14 @@ module.exports = {
   theme: {//Toda vez que eu não tiver uma cor no Tailwild, eu posso gerar uma nova cor aqui e ela já entra na biblioteca do plug-in.
           //não esquecer de color a string para dar a referência ou não irá funcionar!
     extend: {
-      backgroundImage: {
-        galaxy: "url('/background-galaxy.png')", 
-
+      colors: {
         'nlw-gradient': 'linear - gradient(89.86deg, #9572FC 23.08 %, #43E7AD 33.94 %, #E1D55D 44.57 %)',
 
         'game-gradiente': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+
+      },
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')", 
       },
     },
   },
